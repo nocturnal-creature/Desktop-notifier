19045f5cda6fce88531834193b880044acc63dd2
nocturnal-creature <85667186+nocturnal-creature@users.noreply.github.com>
2024-10-29 13:34:19 +0530

    Add files via upload

---
+++ Desktop-notifier.py
@@ -0,0 +1,14 @@
+#import the necessary module!
+from plyer import notification # type: ignore
+
+
+#specify the parameters
+title = 'Hello Amazing people!'
+
+message= 'Thankyou for reading! Take care!'
+
+notification.notify(title= title,
+                    message= message,
+                    app_icon = None,
+                    timeout= 10,
+                    toast=False)
